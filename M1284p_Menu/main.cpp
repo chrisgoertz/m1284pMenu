937fdebef855a4caef093e4ba36f594e3d9ca906
chrisgoertz <chris.goertz@gmx.de>
2024-11-03 22:11:37 +0100

    initial release

---
+++ M1284p_Menu/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * M1284p_Menu.cpp
+ *
+ * Created: 03.11.2024 22:05:52
+ * Author : chris
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
